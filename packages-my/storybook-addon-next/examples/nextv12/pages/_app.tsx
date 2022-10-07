b40d27bfee7a1aaba19f9d09bbc9c7ed066dd496
Paul Shorey <web@artspaces.net>
2022-10-07 01:39:23 -0500

    storybook/next12 integrated

---
+++ packages-my/storybook-addon-next/examples/nextv12/pages/_app.tsx
@@ -0,0 +1,15 @@
+import '../styles/globals.css'
+import '../styles/globals.scss'
+import AppProvider from '@/components/AppProvider'
+import { AppProps } from 'next/app'
+import React from 'react'
+
+const MyApp = ({ Component, pageProps }: AppProps): any => {
+  return (
+    <AppProvider>
+      <Component {...pageProps} />
+    </AppProvider>
+  )
+}
+
+export default MyApp
---
+++ packages-my/storybook-addon-next/src/register.ts
@@ -0,0 +1,3 @@
+import { addons } from '@storybook/addons'
+
+addons.register('storybook-addon-next', () => {})
