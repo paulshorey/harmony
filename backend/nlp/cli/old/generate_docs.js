1445654c1b0dfce55825f4be541fc58b310692ef
Paul Shorey <web@artspaces.net>
2022-12-04 14:23:56 -0600

    remove backend/nlp - will clone as submodule

--- backend/nlp/cli/old/generate_docs.js
+++
@@ -1,8 +0,0 @@
-import "dotenv/config" // contains secret keys ~ never push to GIT!
-import "common/global.js" // contains secret keys ~ never push to GIT!
-;(async function () {
-  // read filesystem
-
-  // exit
-  process.exit()
-})()
--- backend/nlp/src/endpoints/wordio/index.ts
+++
@@ -1,4 +0,0 @@
-import thesaurus from "./endpoints/thesaurus"
-import tokenization from "./endpoints/tokenization"
-
-export default [...thesaurus, ...tokenization]
